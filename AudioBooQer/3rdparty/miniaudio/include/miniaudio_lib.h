9fd84a7868be1915bfe25bb17a0bf0be950f4adc
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2021-04-24 19:31:56 +0200

    New 3rd party library: https://miniaud.io/ .

---
+++ AudioBooQer/3rdparty/miniaudio/include/miniaudio_lib.h
@@ -0,0 +1,13 @@
+#ifndef MINIAUDIO_LIB_H
+#define MINIAUDIO_LIB_H
+
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
+#define MA_NO_ENCODING
+#define MA_NO_FLAC
+#define MA_NO_DEVICE_IO
+#define MA_NO_THREADING
+#define MA_NO_GENERATION
+
+#include <miniaudio.h>
+
+#endif // MINIAUDIO_LIB_H
